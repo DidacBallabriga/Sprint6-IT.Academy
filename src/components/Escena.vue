afcaa8d1106e7e8178e3b4c72bc9bbb3a0b88221
Dídac Ballabriga <didac@ballabriga.com>
2022-03-10 17:12:35 +0100

    main

---
+++ src/components/Escena.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+</template>
+
+
+
+
+<script>
+
+</script>
+
+
+
+
+
+
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+       <escena></escena>
+    </div>
+</template>
+
+
+
+<script>
+import Escena from './Escena.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Escena
+  }
+}
+
+</script>
+
+
+
+
+<style>
+
+</style>
